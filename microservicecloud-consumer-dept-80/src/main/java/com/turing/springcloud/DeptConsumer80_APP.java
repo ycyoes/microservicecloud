d2588d54409c14860dc629d8ead775db389f4ff3
ycyoes <ycyoes@163.com>
2019-12-22 00:15:33 +0800

    新增消费微服务启动类

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/turing/springcloud/DeptConsumer80_APP.java
@@ -0,0 +1,11 @@
+package com.turing.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptConsumer80_APP {
+	public static void main(String[] args) {
+		SpringApplication.run(DeptConsumer80_APP.class, args);
+	}
+}
