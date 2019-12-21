535e3f2e262b66faa6e93d893c94b9acbf8a7173
ycyoes <ycyoes@163.com>
2019-12-21 23:26:33 +0800

    添加配置类-定义RestTemplate

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/turing/springcloud/cfgbean/ConfigBean.java
@@ -0,0 +1,17 @@
+package com.turing.springcloud.cfgbean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+//boot --> spring applicationContext.xml --> @Configuration配置 =  applicationContext.xml
+public class ConfigBean {	
+	@Bean
+	public RestTemplate geTemplate() {
+		return new RestTemplate();
+	}
+}
+
+//applicationContext.xml = ConfigBean(@Configuration)
+//<bean id="userService" class="com.turing.springcloud.UserServiceImpl" />
