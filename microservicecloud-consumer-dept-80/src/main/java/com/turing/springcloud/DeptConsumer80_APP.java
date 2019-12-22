676af37ea84612c7662769011af174f28b219c0b
ycyoes <ycyoes@163.com>
2019-12-22 15:57:57 +0800

    客户端启用Eureka

--- microservicecloud-consumer-dept-80/src/main/java/com/turing/springcloud/DeptConsumer80_APP.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/turing/springcloud/DeptConsumer80_APP.java
@@ -2,8 +2,10 @@ package com.turing.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptConsumer80_APP {
 	public static void main(String[] args) {
 		SpringApplication.run(DeptConsumer80_APP.class, args);
