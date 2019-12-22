902644ae379ad1ec9e46cb7f1a967e7544e79104
ycyoes <ycyoes@xiezhaoangdembp>
2019-12-22 21:55:38 +0800

    启动类指定自定义负载策略使生效

--- microservicecloud-consumer-dept-80/src/main/java/com/turing/springcloud/DeptConsumer80_APP.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/turing/springcloud/DeptConsumer80_APP.java
@@ -3,9 +3,14 @@ package com.turing.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+import com.turing.myrule.MySelfRule;
 
 @SpringBootApplication
 @EnableEurekaClient
+//在启动微服务时就能去加载我们自定义的Ribbon配置类，从而使配置生效
+@RibbonClient(name = "MICROSERVICECLOUD-DEPT", configuration = MySelfRule.class)
 public class DeptConsumer80_APP {
 	public static void main(String[] args) {
 		SpringApplication.run(DeptConsumer80_APP.class, args);
