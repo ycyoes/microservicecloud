2395e10848a8a46cbcddc3696010ed15800a7ab7
ycyoes <ycyoes@163.com>
2019-12-22 10:32:53 +0800

    开启客户端注册到Eureka

--- microservicecloud-provider-dept-8001/src/main/java/com/turing/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/turing/springcloud/DeptProvider8001_App.java
@@ -2,8 +2,10 @@ package com.turing.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient	//本服务启动后会注册到注册中心
 public class DeptProvider8001_App {
 	public static void main(String[] args) {
 		SpringApplication.run(DeptProvider8001_App.class, args);
