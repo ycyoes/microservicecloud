98f84d652604c0d0e1cb3155642f58334c4343ed
ycyoes <ycyoes@163.com>
2019-12-22 11:26:06 +0800

    消费端服务发现

--- microservicecloud-consumer-dept-80/src/main/java/com/turing/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/turing/springcloud/controller/DeptController_Consumer.java
@@ -3,6 +3,7 @@ package com.turing.springcloud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +33,11 @@ public class DeptController_Consumer {
 	@RequestMapping(value = "list")
 	public List<Dept> list() {
 		return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
+	}
+	
+	//测试@EnableDiscoveryClient,消费端可以调用服务发现
+	@RequestMapping(value = "discovery")
+	public Object discovery() {
+		return restTemplate.getForObject(REST_URL_PREFIX + "/dept/discovery", Object.class);
 	}
 }
