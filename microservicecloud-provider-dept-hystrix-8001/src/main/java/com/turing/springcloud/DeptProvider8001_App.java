35001cc8435a67bc34f2355e59cc046b75e4928b
ycyoes <ycyoes@163.com>
2019-12-27 22:31:27 +0800

    启动类中启用熔断器

--- microservicecloud-provider-dept-hystrix-8001/src/main/java/com/turing/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/turing/springcloud/DeptProvider8001_App.java
@@ -2,12 +2,14 @@ package com.turing.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient	//本服务启动后会注册到注册中心
 @EnableDiscoveryClient //服务发现
+@EnableCircuitBreaker	//对hystrix熔断机制的实现
 public class DeptProvider8001_App {
 	public static void main(String[] args) {
 		SpringApplication.run(DeptProvider8001_App.class, args);
